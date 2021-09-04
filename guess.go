a19d457fcd605939794c7f0a81388b458b887cc5
alloyking1 <alloyking1@gmail.com>
2021-09-04 17:44:26 -0400

    created a simple guessing game in go

---
+++ guess.go
@@ -0,0 +1,35 @@
+package main
+import "fmt"
+
+
+
+
+
+
+func main () {
+	gamePlay()
+}
+
+func gamePlay () {
+
+	var score int
+	var guess int
+	var correctNumber int = 3
+
+	
+
+	for guess != correctNumber {
+		fmt.Println("Guess a number between 1-5: ")
+		fmt.Scanln(&guess)
+
+		if guess == correctNumber {
+
+			score ++
+			fmt.Printf("%d is the correct number. You'r score is %d", guess, score)
+			
+		}
+	}
+
+	
+
+}

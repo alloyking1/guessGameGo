6c07c1e98f0ee77b846cae00a5e666658ef7b24d
alloyking1 <alloyking1@gmail.com>
2021-09-04 20:31:57 -0400

    got game to work

--- guess.go
+++ guess.go
@@ -1,5 +1,9 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 
 func main () {
@@ -9,12 +13,15 @@ func main () {
 func gamePlay () {
 
 	var score int
-	var guess int
-	var correctNumber int = 3
+	var guess int 
+	var correctNumber int 
 
-	
+    correctNumber = randGen()
+
+	fmt.Println(correctNumber)
 
 	for guess != correctNumber {
+		
 		fmt.Println("Guess a number between 1-5: ")
 		fmt.Scanln(&guess)
 
@@ -22,10 +29,23 @@ func gamePlay () {
 
 			score ++
 			fmt.Printf("%d is the correct number. You'r score is %d", guess, score)
+
+			for guess == correctNumber {
+
+				correctNumber = randGen()
+				fmt.Scanln(&guess)
+				
+			}
 			
 		}
 	}
 
-	
-
 }
+
+func randGen () int {
+	rand.Seed(time.Now().UnixNano())
+    min := 1
+    max := 5
+	return rand.Intn(max - min + 1) + min
+}
+
